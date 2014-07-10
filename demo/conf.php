6e3af3a61cf4f8aeff1e91ecd9ff2c5a819b53df
Jerry Jäppinen <eiskis@gmail.com>
2014-07-10 13:44:06 +0300

    Added test durations to reports

--- demo/conf.php
+++ demo/conf.php
@@ -1,9 +1,4 @@
 <?php
 $specPath = '../tests/';
-
-$injections = array(
-	'foo' => 1,
-	'bar' => 2,
-	'string' => 'Some string value',
-);
+$injections = array();
 ?>
--- tests/injections/basic.php
+++ tests/injections/basic.php
@@ -2,18 +2,47 @@
 
 class UnitestInjectionsBasic extends Unitest {
 
-	function testOneInjection ($foo) {
+	function init () {
+		$this->inject('foo', 1)->inject('bar', 2)->inject('string', 'Some string value');
+	}
+
+	// FLAG injecting in test methods leaves injections for other injections
+	function testLocalInjection () {
+		$this->inject('testInjection', 3);
+		return $this->should($this->injection('testInjection') === 3);
+	}
+
+	function testInjectionOfAnotherMethodIsNotAvailable () {
+		try {
+			$this->injection('testInjection');
+			return $this->fail();
+		} catch (Exception $e) {
+			return $this->pass();
+		}
+	}
+
+	function testOverWrittenLocalInjection () {
+		$this->inject('anotherTestInjection', 1);
+		$this->inject('anotherTestInjection', 2);
+		return $this->should($this->injection('anotherTestInjection') === 2);
+	}
+
+	function testClassInitInjectionCount ($foo) {
 		$arguments = func_get_args();
 		return $this->should(count($arguments) === 1);
 	}
 
-	function testTwoInjections ($foo, $bar) {
+	function testTwoSelfInjectionsCount ($foo, $bar) {
 		$arguments = func_get_args();
 		return $this->should(count($arguments) === 2);
 	}
 
-	function testRightInjection ($string) {
-		return $this->should(isset($string) and is_string($string));
+	function testClassInitInjectionValue ($foo) {
+		return $this->should($foo === 1);
+	}
+
+	function testTwoSelfInjectionsValues ($foo, $bar) {
+		return $this->should(($foo === 1), ($bar === 2));
 	}
 
 	function testUnavailableInjection () {
